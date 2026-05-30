4f0185aa2ea2b84843b54d4bbacbd4347c72fd48
Surroz <breakingbad979@gmail.com>
2026-05-30 21:05:12 +0300

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int prev = 1;
+        int pprev = 1;
+        int res = 1;
+        for (int i = 2; i <= n; i++) {
+            res = prev + pprev;
+            pprev = prev;
+            prev = res;
+        }
+        return res;
+    }
+}
